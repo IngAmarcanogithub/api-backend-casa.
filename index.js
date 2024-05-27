c1b6e14cb98035325a0bb9ff1fb8903ed7728cd8
Angel Marcano <marcanoap1980@gmail.com>
2024-05-26 20:11:07 -0400

    se agregaron archivos de produccion dist

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-
+app.use(express.static('dist'))
 const cors = require('cors')
-
 app.use(cors())
+
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
